2e359c36d1315b8f1a4f3937a888c7a172c7c9aa
Yakir Gagnon <12.yakir@gmail.com>
2024-03-20 15:02:45 +0100

    plot

--- main.jl
+++ main.jl
@@ -1,6 +1,18 @@
 using Dates
 using Autotrack
 using CSV, DataFrames
+using CairoMakie
+
+function plotit(t1, t2, spl, ar, name)
+    ts = range(t1, t2, 100)
+    xy = [Point2f(ar .* spl(t)) for t in ts]
+    fig = Figure()
+    ax = Axis(fig[1,1], aspect=DataAspect())
+    lines!(ax, xy)
+    hidedecorations!(ax)
+    hidespines!(ax)
+    save("$name.eps", fig)
+end
 
 tosecond(t::T) where {T} = t / convert(T, Dates.Second(1))
 
@@ -9,5 +21,6 @@ transform!(df, [:start, :stop] .=> ByRow(x -> tosecond(x - Time(0))); renamecols
 
 for (i, row) in enumerate(eachrow(df))
     name = string(i)
-    track(row.file, row.start, row.stop; csv_file=name, debug_file=name, temporal_step=0.5)
+    t1, t2, spl, ar = track(row.file, row.start, row.stop; csv_file=name, debug_file=name, temporal_step=0.5)
+    plotit(t1, t2, spl, ar, name)
 end
